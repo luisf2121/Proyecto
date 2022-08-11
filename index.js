ea13a730f8213df4a421467c99fd36a6ec54b865
Fernando Gonzales <luisfernantkm@gmail.com>
2022-08-10 21:36:44 -0500

    modificando barra de navegacion y agregando algunos estilos

---
+++ index.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".nav-toggle1")
+const navMenu = document.querySelector(".nav-menu1")
+
+navToggle.addEventListener("click", () => {
+navMenu.classList.toggle("nav-menu1-visible");
+});
